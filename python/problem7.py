8d0372032a1e557a853f69d8bbf8631668215d03
ShowLo <945519267@qq.com>
2017-03-18 13:29:55 +0800

    add problem7

---
+++ python/problem7.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        s = str(abs(x));
+        if x >= 0:
+            r = int(s[::-1]);
+        else:
+            r = -int(s[::-1]);
+        if r > 2**31 - 1 or r < -2**31:
+            return 0;
+        else:
+            return r;
