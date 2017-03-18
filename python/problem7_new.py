3005b57d52754b3216c74675b6c7bbd37a534b88
ShowLo <945519267@qq.com>
2017-03-18 13:47:01 +0800

    new solution for problem7

---
+++ python/problem7_new.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        temp = abs(x);
+        result = 0;
+        while  temp:
+            r = temp % 10;
+            temp = temp // 10;
+            result = result * 10 + r;
+        if x < 0:
+            result = - result;
+        if result > 2**31 - 1 or result < -2**31:
+            return 0;
+        return result;
