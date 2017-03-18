7912758362e73a8bca46e2c06a46067dfb47b550
ShowLo <945519267@qq.com>
2017-03-18 15:06:13 +0800

    add problem6

---
+++ python/problem6.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s;
+        result = "";
+        temp = ["" for n in range(numRows)];
+        for i in range(len(s)):
+            t = i % (numRows * 2 - 2);
+            index = numRows - 1 - abs(t - numRows + 1);
+            temp[index] += s[i];
+        for n in range(numRows):
+            result += temp[n];
+        return result;
