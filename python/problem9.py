3fc7d78d4194a703c6991e4724ccc2ef617d12ec
ShowLo <945519267@qq.com>
2017-03-19 13:14:43 +0800

    add problem9

---
+++ python/problem9.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False;
+        reverse = 0;
+        temp = x;
+        while  temp:
+            reverse = reverse * 10 + temp % 10;
+            temp = temp // 10;
+        return x == reverse;
---
+++ python/problem9_new.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False;
+        digit = 1;
+        while x//digit:
+            digit *= 10;
+        digit = digit // 10;
+        while x:
+            left = x // digit;
+            right = x % 10;
+            if left != right:
+                return False;
+            x -= left * digit;
+            x = x // 10;
+            digit = digit // 100;
+        return True;
