03dc7105915ce4e0e826e7bdde84883d1788f72b
ShowLo <945519267@qq.com>
2017-03-21 10:43:03 +0800

    add problem11

---
+++ python/problem5.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        length = len(s);
+        maxLen = 0;
+        str = "";
+        for i in range(length):
+            for j in range(min(i + maxLen + 1,length),length + 1):
+                substr = s[i:j];
+                if substr == substr[::-1]:
+				    if j - i > maxLen:
+				        maxLen = j - i;
+				        str = substr;
+        return str;
