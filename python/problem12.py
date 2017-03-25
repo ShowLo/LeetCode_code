988eac53a0596c9ca9296da6f2f5899a03fa7ab4
ShowLo <945519267@qq.com>
2017-03-25 13:31:17 +0800

    add problem12

---
+++ python/problem12.py
@@ -0,0 +1,11 @@
+﻿class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        ones = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
+        tens = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'];
+        hundreds = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'];
+        thousands = ['','M','MM','MMM'];
+        return thousands[num // 1000] + hundreds[(num % 1000) // 100] + tens[(num % 100) // 10] + ones[num % 10];
