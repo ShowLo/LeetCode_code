5d00216b1ecc550d6aae36ef5787a0c0c835029a
ShowLo <945519267@qq.com>
2017-03-26 13:19:06 +0800

    add problem13

---
+++ python/problem13.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        romanInt = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000};
+        num = romanInt[s[0]];
+        for i in range(1,len(s)):
+            if romanInt[s[i]] > romanInt[s[i - 1]]:
+                num += romanInt[s[i]] - 2 * romanInt[s[i - 1]];
+            else:
+                num += romanInt[s[i]];
+        return num;
