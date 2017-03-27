8e5209d0b1185d0cbfa14d62de9074320ef1ae3d
ShowLo <945519267@qq.com>
2017-03-27 18:57:49 +0800

    add problem14

---
+++ python/problem14.py
@@ -0,0 +1,26 @@
+﻿class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        length = len(strs);
+        if length == 0:
+            return '';
+        if length == 1:
+            return strs[0];
+        flag = True;
+        maxLen = 0;
+        while flag:
+            for i in range(length - 1):
+                if len(strs[i]) == 0 or len(strs[i + 1]) == 0:
+                    return '';
+                elif strs[i][maxLen] != strs[i + 1][maxLen]:
+                    flag = False;
+                    break;
+                else:
+                    if len(strs[i]) == maxLen + 1 or len(strs[i + 1]) == maxLen + 1:
+                        flag = False;
+                    if i == length - 2:
+                            maxLen += 1;
+        return strs[0][0:maxLen];
