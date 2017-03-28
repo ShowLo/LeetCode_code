503e889c480f8e3a4d378f13b8759ef43120631a
ShowLo <945519267@qq.com>
2017-03-28 15:14:42 +0800

    new solution for problem1

---
+++ python/problem1.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums);
+        differDict = {};
+        for i in range(length):
+            if differDict.get(nums[i]) == None:
+                differDict[target - nums[i]] = i;
+            else:
+                return [differDict[nums[i]],i];
