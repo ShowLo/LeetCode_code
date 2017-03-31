53036fd75bc424d839352c6c48c0c09a85a8541c
ShowLo <945519267@qq.com>
2017-03-31 22:44:19 +0800

    modify the format

--- python/problem1.py
+++ python/problem1.py
@@ -6,11 +6,11 @@
         :rtype: List[int]
         """
         length = len(nums);
-        differDict = {};				#用字典存放差值--索引对
+        differDict = {};                #用字典存放差值--索引对
         for i in range(length):
             num = nums[i];
-            item = differDict.get(num);	#看当前值是否已在差值--索引字典当中
-            if item == None:			#如果不在的话，将目标值与当前值作为key，索引作为value
+            item = differDict.get(num); #看当前值是否已在差值--索引字典当中
+            if item == None:            #如果不在的话，将目标值与当前值作为key，索引作为value
                 differDict[target - num] = i;
-            else:						#存在的话，直接返回
+            else:                       #存在的话，直接返回
                 return [item,i];
