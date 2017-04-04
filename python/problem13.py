7e59f9e10043db7af896631fed5c67399569fa8f
ShowLo <945519267@qq.com>
2017-04-04 14:21:22 +0800

    modify

--- python/problem13.py
+++ python/problem13.py
@@ -6,7 +6,7 @@
         """
         romanInt = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000};#各罗马数字所对应阿拉伯数字
         num = romanInt[s[0]];
-        for i in range(1,len(s)):										#根据罗马数字和阿拉伯数字对应关系进行转换
+        for i in range(1,len(s)):                                       #根据罗马数字和阿拉伯数字对应关系进行转换
             if romanInt[s[i]] > romanInt[s[i - 1]]:
                 num += romanInt[s[i]] - 2 * romanInt[s[i - 1]];
             else:
