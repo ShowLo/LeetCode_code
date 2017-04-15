d6ddcf9edfd3718a0e78df4757ad4e105371c66c
ShowLo <945519267@qq.com>
2017-04-15 14:31:36 +0800

    add problem26

---
+++ python/problem26.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 1;
+        length = len(nums);
+        if length == 0:
+            return 0;
+        if length == 1:
+            return 1;
+        for i in range(1,length):
+            if nums[i] == nums[i - 1]:  #当前元素与前一个元素一样，跳过
+                continue;
+            else:                       #否则的话说明有一个新的数
+                nums[result] = nums[i];
+                result += 1;
+        return result;
