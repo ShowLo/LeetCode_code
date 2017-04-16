56760f4bf62f84bb5f09e5c42ed3a2c0cec802bf
ShowLo <945519267@qq.com>
2017-04-16 14:16:14 +0800

    add problem27

---
+++ python/problem27.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = len(nums);
+        result = 0;
+        for i in range(length):
+            if nums[i] == val:
+                continue;
+            else:
+                nums[result] = nums[i];
+                result += 1;
+        return result;
