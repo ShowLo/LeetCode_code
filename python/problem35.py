114ef6b02af36cbc3094689422e8ef9cf8611faa
ShowLo <945519267@qq.com>
2017-04-19 19:19:07 +0800

    add problem35

---
+++ python/problem35.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        length = len(nums);
+        if target <= nums[0]:
+            return 0;
+        if target > nums[-1]:
+            return length;
+        for i in range(length):
+            if nums[i] >= target:
+                return i;
