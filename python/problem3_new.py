573c53a78a341e482931905283556b409136bf7f
ShowLo <945519267@qq.com>
2017-04-29 15:20:45 +0800

    improve the algorithm

--- python/problem3_new.py
+++ python/problem3_new.py
@@ -12,7 +12,7 @@
             if s[right] in charDict and left <= charDict[s[right]]: #出现重复的字符且这个字符在窗口中
                 left = charDict[s[right]] + 1;                      #左窗口位置调整到这个重复字符的右端
             else:                                                   #没有重复字符出现
-                charDict[s[right]] = right;                         #更新字符位置
                 maxLen = max(maxLen,right - left + 1);              #更新最长长度
-                right += 1;                                         #右窗口后移
+            charDict[s[right]] = right;                             #更新字符位置
+            right += 1;                                             #右窗口后移
         return maxLen;
