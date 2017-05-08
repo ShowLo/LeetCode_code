7d1a3dda54533c42da67ccc1f87cf63265485439
ShowLo <945519267@qq.com>
2017-05-08 22:23:08 +0800

    add problem3
    change something

--- Java/problem3.java
+++ Java/problem3.java
@@ -5,19 +5,19 @@
         int lenS = s.length();
         int maxLen = 0;
         int left = 0, right = 0, position = 0;
+        char c;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         while(right < lenS)
         {
-            if(map.containsKey(s.charAt(right)))                //出现重复的字符且这个字符在窗口中
+            c = s.charAt(right);
+            if(map.containsKey(c) && left <= map.get(c))                        //出现重复的字符且这个字符在窗口中
             {
-                position = map.get(s.charAt(right));
-                if(left <= position)
-                    left = position + 1;                        //左窗口位置调整到这个重复字符的右端
+                left = map.get(c) + 1;                                          //左窗口位置调整到这个重复字符的右端
             }
-            else                                                //没有重复字符出现
-                maxLen = maxLen > right - left + 1 ? maxLen : right - left + 1;      //更新最长长度
-            map.put(s.charAt(right), right);                    //更新字符位置
-            ++right;                                            //右窗口后移
+            else                                                                //没有重复字符出现
+                maxLen = maxLen > right - left + 1 ? maxLen : right - left + 1; //更新最长长度
+            map.put(c, right);                                                  //更新字符位置
+            ++right;                                                            //右窗口后移
         }
         return maxLen;
     }
