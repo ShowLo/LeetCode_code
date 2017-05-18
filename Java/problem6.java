f2417bd2053da8bd707b0ab2aabfd1e1a53de481
ShowLo <945519267@qq.com>
2017-05-18 16:44:50 +0800

    add problem6 using java

---
+++ Java/problem6.java
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public String convert(String s, int numRows) 
+    {
+        if(numRows == 1)
+            return s;
+        StringBuilder result = new StringBuilder();
+        StringBuilder[] temp = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; ++i)
+            temp[i] = new StringBuilder();
+        int t,index;
+        for(int i = 0; i < s.length(); ++i)
+        {
+            t = i % (numRows * 2 - 2);
+            index = numRows - 1 - abs(t - numRows + 1);
+            temp[index].append(s.charAt(i));
+        }
+        for(int i = 0; i < numRows; ++i)
+            result.append(temp[i]);
+        return result.toString();
+    }
+    private int abs(int n)
+    {
+        return n > 0 ? n :-n;
+    }
+}
