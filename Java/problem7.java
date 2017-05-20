64208e4911b9162a4e2b36134ce9731432e102ca
ShowLo <945519267@qq.com>
2017-05-20 12:08:56 +0800

    add problem7

---
+++ Java/problem7.java
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int reverse(int x) 
+    {
+        int temp = x;
+        int r, result = 0, newResult = 0;
+        while(temp != 0)
+        {
+            r = temp % 10;
+            temp = temp / 10;
+            newResult = result * 10 + r;
+            if((newResult - r)/10 != result)        //溢出
+                return 0;
+            result = newResult;
+        }
+        return result;
+    }
+}
