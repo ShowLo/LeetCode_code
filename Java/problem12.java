48d5f2e45816af1c57416928d5e8956a145b1b38
ShowLo <945519267@qq.com>
2017-05-26 12:06:10 +0800

    add problem12

---
+++ Java/problem12.java
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public String intToRoman(int num)
+    {
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};     //个位数所对应罗马数字
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};     //十位数所对应罗马数字
+        String[] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"}; //百位数所对应罗马数字
+        String[] thousands = {"","M","MM","MMM"};                                //千位数所对应罗马数字
+        return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+    }
+}
