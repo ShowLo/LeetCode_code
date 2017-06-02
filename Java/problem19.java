6843b94a344118969dc1ac8b89adaee139bf77f0
ShowLo <945519267@qq.com>
2017-06-02 11:40:43 +0800

    add problem19

---
+++ Java/problem19.java
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public ListNode removeNthFromEnd(ListNode head, int n)
+    {
+        ListNode font = head, tail = head;
+        for(int i = 0; i < n; ++i)      //移动使font和tail前后两节点相隔n
+            tail = tail.next;
+        if(tail == null)                //要移除的是头节点
+            return head.next;
+        while(tail.next != null)        //后节点移到最后
+        {
+            tail = tail.next;
+            font = font.next;
+        }
+        font.next = font.next.next;     //删除倒数第n个节点
+        return head;
+    }
+}
