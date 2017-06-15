27690f4501df3da9b260b8764857aea8c61cd230
ShowLo <945519267@qq.com>
2017-06-15 13:35:47 +0800

    add problem26

---
+++ Java/problem26.java
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int removeDuplicates(int[] nums) 
+    {
+        int result = 1;
+        int length = nums.length;
+        if(length == 0)
+            return 0;
+        if(length == 1)
+            return 1;
+        for(int i = 1; i < length; ++i)
+        {
+            if(nums[i] == nums[i - 1])  //当前元素与前一个元素一样，跳过
+                continue;
+            else                        //否则的话说明有一个新的数
+            {
+                nums[result] = nums[i];
+                result += 1;
+            }
+        }
+        return result;
+    }
+}
