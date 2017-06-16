177aeb470c2d7cd87ac86c9748bf1f62d25ea7eb
ShowLo <945519267@qq.com>
2017-06-16 11:43:01 +0800

    add problem27

---
+++ Java/problem27.java
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        int length = nums.length;
+        int result = 0;
+        for(int i = 0; i < length; ++i)
+        {
+            if(nums[i] == val)
+                continue;
+            else
+            {
+                nums[result] = nums[i];
+                ++result;
+            }
+        }
+        return result;
+    }
+}
