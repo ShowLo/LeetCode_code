9e15f561197cd35e32144b85a4b80b2b8e1c31f6
ShowLo <945519267@qq.com>
2017-06-25 16:06:04 +0800

    add problem35

---
+++ Java/problem35.java
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        int length = nums.length;
+        if(target <= nums[0])
+            return 0;
+        
+        for(int i = 0; i < length; ++i)
+        {
+            if(nums[i] >= target)
+                return i;
+        }
+        return length;
+    }
+}
