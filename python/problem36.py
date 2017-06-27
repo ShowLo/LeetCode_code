bc6052c4ba350196fb833abec98bf85a5bda4c52
ShowLo <945519267@qq.com>
2017-06-27 20:01:19 +0800

    add problem36

---
+++ python/problem36.py
@@ -0,0 +1,31 @@
+﻿class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in range(9):
+            mark = [0] * 9;
+            for j in range(9):
+                if board[i][j] != '.':                  #先对每一行作判断
+                    mark[(int)(board[i][j])-1] += 1;
+                    if mark[(int)(board[i][j])-1] == 2:
+                        return False;
+            
+            markCol = [0] * 9;
+            for j in range(9):
+                if board[j][i] != '.':                  #再对每一列作判断
+                    markCol[(int)(board[j][i])-1] += 1;
+                    if markCol[(int)(board[j][i])-1] == 2:
+                        return False;
+        
+        for i in range(3):                              #对每一个方格作判断
+            for j in range(3):
+                mark = [0] * 9;
+                for k in range(3):
+                    for m in range(3):
+                        if board[i*3 + k][j*3 + m] != '.':
+                            mark[(int)(board[i*3 + k][j*3 + m])-1] += 1;
+                            if mark[(int)(board[i*3 + k][j*3 + m])-1] == 2:
+                                return False;
+        return True;
