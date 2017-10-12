8d712f05db99aea3c5eb94988240a4659164c51f
ShowLo <945519267@qq.com>
2017-10-12 17:13:54 +0800

    add problem46

---
+++ python/problem46.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        import itertools
+        permutation = itertools.permutations(range(len(nums)), len(nums))
+        result = []
+        for index in permutation:
+            temp = []
+            for i in index:
+                temp.append(nums[i])
+            result.append(temp)
+        return result
