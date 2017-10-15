42c8831813dc5933fd17c9604cbed0aaa7a7e64d
ShowLo <945519267@qq.com>
2017-10-15 16:09:46 +0800

    add problem48

---
+++ python/problem48.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        size = len(matrix)
+        #先沿对角线翻转
+        for row in range(size):
+            for col in range(size - row):
+                matrix[row][col], matrix[size - 1 - col][size - 1 - row] = matrix[size - 1 - col][size - 1 - row], matrix[row][col]
+        #再沿中线翻转
+        for row in range(size//2):
+            for col in range(size):
+                matrix[row][col], matrix[size - 1 - row][col] = matrix[size - 1 - row][col], matrix[row][col]
