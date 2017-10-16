65a781a88d90c78034c3e20599dabec64402aaa5
ShowLo <945519267@qq.com>
2017-10-16 15:46:16 +0800

    add problem49

---
+++ python/problem49.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        strDict = {}
+        for str in strs:
+            s = list(str)
+            s.sort()
+            sortStr = "".join(s)
+            if sortStr not in strDict:
+                strDict[sortStr] = [str]
+            else:
+                strDict[sortStr].append(str)
+        retList = []
+        for value in strDict.values():
+            retList.append(value)
+        return retList
