f9370d68e49513d89fa53de65caefb4c6d55fd88
ShowLo <945519267@qq.com>
2017-10-17 15:33:20 +0800

    add problem50

---
+++ python/problem50.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        sign = n < 0
+        m = abs(n) // 2
+        if sign:
+            m = -m
+        half = self.myPow(x, m)
+        if n % 2 == 0:
+            return half * half
+        elif n > 0:
+            return half * half * x
+        else:
+            return half * half / x
