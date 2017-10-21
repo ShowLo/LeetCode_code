205c76d44bf63622f32e53b7185fc127b23a16cc
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-10-21 11:47:43 +0800

    add problem53

---
+++ python/problem53.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxSum = float('-inf')
+        tempSum = 0
+        for n in nums:
+            if tempSum >= 0:
+                tempSum += n
+            else:
+                tempSum = n
+            if tempSum > maxSum:
+                maxSum = tempSum
+        return maxSum
