2a6109a314a3d99791957df4875a20cf9ca980e4
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-10-24 12:43:29 +0800

    add problem55

---
+++ python/problem55.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        numsLen = len(nums)
+        start = 0
+        end = 0
+        while start <= end:
+            temp = start + nums[start]
+            if temp > end:
+                end = temp
+            if end >= numsLen - 1:
+                return True
+            start += 1
+        return False
