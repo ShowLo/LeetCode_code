c4a3c523f26818a4157c07619b2509bf5fcac134
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-10-25 19:52:36 +0800

    add problem56

---
+++ python/problem56.py
@@ -0,0 +1,29 @@
+﻿# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        num = len(intervals)
+        if num <= 1:
+            return intervals
+        intervals.sort(key = lambda obj : obj.start)
+        start = intervals[0].start
+        end = intervals[0].end
+        result = []
+        for i in range(1, num):
+            if intervals[i].start <= end:
+                if end < intervals[i].end:
+                    end = intervals[i].end
+            else:
+                result.append(Interval(start, end))
+                start = intervals[i].start
+                end = intervals[i].end
+        result.append(Interval(start, end))
+        return result
