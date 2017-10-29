da690dfdb67e17db07b289ffb25435023c612c43
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-10-29 14:13:28 +0800

    add problem60

---
+++ python/problem60.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        num = 1
+        for i in range(2, n):       #计算(n-1)!
+            num *= i
+        oneToN = [str(i) for i in range(1, n + 1)]
+        result = []
+        count = n - 1
+        while count > 0:
+            index = (k - 1) // num
+            k = k % num
+            result.append(oneToN[index])
+            oneToN.pop(index)
+            num /= count
+            count -= 1
+        result.append(oneToN[0])
+        return ''.join(result)
