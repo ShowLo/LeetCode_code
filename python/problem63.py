b275e9f512d95d32eee576010966ee5f5de73ce0
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-01 10:58:58 +0800

    add problem63

---
+++ python/problem63.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        paths = [[0] * n for i in range(m)]
+        for i in range(n):
+            if obstacleGrid[0][i] != 1:
+                paths[0][i] = 1
+            else:
+                break
+        for i in range(m):
+            if obstacleGrid[i][0] != 1:
+                paths[i][0] = 1
+            else:
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] != 1:
+                    paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
+        return paths[m - 1][n - 1]
