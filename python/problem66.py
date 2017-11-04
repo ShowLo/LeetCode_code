d53233cf0df5d2f142f9c9cdd5f9c5f83db29020
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-04 15:49:15 +0800

    add problem66

---
+++ python/problem66.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = len(digits)
+        temp = 0
+        for i in range(num - 1, -1, -1):
+            temp = digits[i] + 1
+            if temp < 10:
+                digits[i] = temp
+                return digits
+            else:
+                digits[i] = temp % 10
+        if temp >= 10:
+            digits.insert(0, 1)
+        return digits
