41b35f6364e64404e144e71009c165626d9ec63c
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-06 11:48:40 +0800

    add problem69

---
+++ python/problem69.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left = 0
+        right = x // 2 + 1
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid ** 2
+            if square == x:
+                return mid
+            elif square < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
