2a76123cf635cf5b0c972be61498034897b708d4
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-07 12:07:28 +0800

    add problem70

---
+++ python/problem70.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = 1
+        y = 1
+        t = 1
+        #其实就是斐波那契数列的计算
+        while t < n:
+            x, y = y, x + y
+            t += 1
+        return y
