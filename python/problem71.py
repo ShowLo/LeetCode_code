d5c7adfcbdbf9b545beb2fece6877816f0055019
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-09 15:41:20 +0800

    add problem71

---
+++ python/problem71.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        pathLen = len(path)
+        index = 0
+        while index < pathLen:
+            while index < pathLen and path[index] == '/':
+                index += 1
+            s = ''
+            while index < pathLen and path[index] != '/':
+                s += path[index]
+                index += 1
+            if s == '..':
+                if len(stack) != 0:
+                    stack.pop()
+            elif s != '.' and s != '':
+                stack.append(s)
+        return '/' + '/'.join(stack)
