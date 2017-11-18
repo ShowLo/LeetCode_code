bbf27d25cb621926dd3cad7ccec43ae5ffedbf0c
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-11-18 14:44:59 +0800

    add problem75

---
+++ python/problem75.py
@@ -0,0 +1,29 @@
+﻿class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = len(matrix)
+        if row == 0:
+            return False
+        col = len(matrix[0])
+        if col == 0:
+            return False
+        targetRow = -1
+        for i in range(row):
+            if matrix[i][0] > target:
+                targetRow = i - 1
+                break
+            elif matrix[i][col - 1] >= target:
+                targetRow = i
+                break
+        if targetRow == -1:
+            return False
+        for i in range(col):
+            if matrix[targetRow][i] == target:
+                return True
+            elif matrix[targetRow][i] > target:
+                return False
+        return False
