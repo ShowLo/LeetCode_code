81411135c3c873563ba6f4e171ed4d6e142cc328
C.J.John <cjr14@mails.tsinghua.edu.cn>
2017-12-15 11:20:32 +0800

    add problem80

---
+++ python/problem80.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mark = set()
+        count = 0
+        index = 0
+        numsLen = len(nums)
+        while index < numsLen:
+            if nums[index] in mark:
+                count += 1
+                if count <= 2:
+                    index += 1
+                else:
+                    nums.pop(index)
+                    numsLen -= 1
+            else:
+                mark.add(nums[index])
+                count = 1
+                index += 1
+        return index
