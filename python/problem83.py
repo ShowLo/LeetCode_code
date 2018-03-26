1e5cb7b853699fc5f33460e2b51a8aed6105edd5
ShowLo <945519267@qq.com>
2018-03-26 10:48:36 +0800

    add problem83

---
+++ python/problem83.py
@@ -0,0 +1,21 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return
+        node = head
+        while node.next != None:
+            if node.next.val == node.val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        return head
