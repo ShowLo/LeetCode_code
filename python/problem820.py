4367360d3c3d5b56aed03fd5659713d2e4ad1d3c
ShowLo <945519267@qq.com>
2020-03-28 17:11:03 +0800

    Create problem820.py

---
+++ python/problem820.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        wordSet = set(words)
+        for word in words:
+            for i in range(1, len(word)):
+                wordSet.discard(word[i:])
+        return sum([len(word) + 1 for word in wordSet])
