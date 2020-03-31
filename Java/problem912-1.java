38bfb9134f40f6582f007884fccef7591c2cbde5
ShowLo <945519267@qq.com>
2020-03-31 17:06:35 +0800

    Create problem912-1.java

---
+++ Java/problem912-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    private void swap(int[] nums, int i, int j) {
+        if (i != j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int i = left - 1;
+        int pivot = nums[right];
+        for (int j = left; j < right; ++j) {
+            if (nums[j] < pivot) {
+                swap(nums, ++i, j);
+            }
+        }
+        swap(nums, ++i, right);
+        return i;
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int middle = partition(nums, left, right);
+        quickSort(nums, left, middle - 1);
+        quickSort(nums, middle + 1, right);
+    }
+
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
