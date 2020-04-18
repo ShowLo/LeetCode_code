2f9bb0b396d496f409172a7a607afd063ac66275
ShowLo <945519267@qq.com>
2020-04-18 11:47:48 +0800

    Update interview_10_1.java

--- Java/interview_10_1.java
+++ Java/interview_10_1.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int F0 = 0, F1 = 1;
+        for (int i = 2; i <= n; ++i) {
+            int temp = F1;
+            F1 = (F0 + F1) % 1000_000_007;
+            F0 = temp;
+        }
+        return F1;
+    }
+}
