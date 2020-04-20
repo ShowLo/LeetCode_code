c834b2fea687106568fc58779c50ed82177224c4
ShowLo <945519267@qq.com>
2020-04-20 21:13:29 +0800

    Delete interview_10_1.py

--- interview_10_1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
-        F0, F1 = 0, 1
-        for _ in range(2, n + 1):
-            F0, F1 = F1, (F0 + F1) % 1000000007
-        return F1
