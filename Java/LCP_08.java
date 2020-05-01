5987c67a6dd10f2b58b561b159def83a7e909117
ShowLo <945519267@qq.com>
2020-05-01 22:39:15 +0800

    Update LCP_08.java

--- Java/LCP_08.java
+++ Java/LCP_08.java
@@ -12,34 +12,32 @@ class Solution {
         int[] c = new int[increase[N - 1][0] + 1];
         int[] r = new int[increase[N - 1][1] + 1];
         int[] h = new int[increase[N - 1][2] + 1];
-        int[] count = new int[] {1, 1, 1};
+        int count = 1;
         for (int i = 1; i <= increase[0][0]; ++i) {
-            c[i] = count[0];
+            c[i] = count;
         }
         for (int i = 1; i <= increase[0][1]; ++i) {
-            r[i] = count[1];
+            r[i] = count;
         }
         for (int i = 1; i <= increase[0][2]; ++i) {
-            h[i] = count[2];
+            h[i] = count;
         }
         for (int i = 1; i < N; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                ++count[j];
-            }
+            ++count;
             // 注意如果increase为0的话就不用修改次数了
             if (increase[i][0] > increase[i - 1][0]) {
                 for (int j = increase[i][0]; j > 0 && c[j] == 0; --j) {
-                    c[j] = count[0];
+                    c[j] = count;
                 }
             }
             if (increase[i][1] > increase[i - 1][1]) {
                 for (int j = increase[i][1]; j > 0 && r[j] == 0; --j) {
-                    r[j] = count[1];
+                    r[j] = count;
                 }
             }
             if (increase[i][2] > increase[i - 1][2]) {
                 for (int j = increase[i][2]; j > 0 && h[j] == 0; --j) {
-                    h[j] = count[2];
+                    h[j] = count;
                 }
             }
         }
