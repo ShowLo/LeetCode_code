d855d6911e67de7fb4d8b2d537cd96f90611f762
ShowLo <945519267@qq.com>
2020-05-27 20:04:29 +0800

    Create problem1456.java

---
+++ Java/problem1456.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int[] index = new int[26];
+        Arrays.fill(index, -1);
+        index['a' - 'a'] = 0;
+        index['e' - 'a'] = 1;
+        index['i' - 'a'] = 2;
+        index['o' - 'a'] = 3;
+        index['u' - 'a'] = 4;
+        char[] chars = s.toCharArray();
+        int[] count = new int[5];
+        int left = 0, right = 0;
+        int sum = 0;
+        for (; right < k; ++right) {
+            int idx = index[chars[right] - 'a'];
+            if (idx!= -1) {
+                ++count[idx];
+                ++sum;
+            }
+        }
+        int result = sum;
+        for (; right < chars.length; ++right) {
+            int leftIdx = index[chars[left] - 'a'];
+            int rightIdx = index[chars[right] - 'a'];
+            
+            if (leftIdx != -1 && rightIdx != -1) {
+                ++count[rightIdx];
+                --count[leftIdx];
+            }
+            else if (leftIdx != -1) {
+                --count[leftIdx];
+                --sum;
+            }
+            else if (rightIdx != -1) {
+                ++count[rightIdx];
+                ++sum;
+                result = Math.max(result, sum);
+            }
+            ++left;
+        }
+        return result;
+    }
+}
