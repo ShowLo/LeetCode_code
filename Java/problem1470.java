3a19fad8876bd63f940e43c9f34bcb0dc8312b4c
ShowLo <945519267@qq.com>
2020-06-10 18:33:49 +0800

    Update problem1470.java

--- Java/problem1470.java
+++ Java/problem1470.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] result = new int[nums.length];
-        int half = nums.length / 2;
-        for (int i = 0; i < half; ++i) {
+        int[] result = new int[n * 2];
+        for (int i = 0; i < n; ++i) {
             result[i * 2] = nums[i];
-            result[i * 2 + 1] = nums[i + half];
+            result[i * 2 + 1] = nums[i + n];
         }
         return result;
     }
