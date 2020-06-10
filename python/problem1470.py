d2326e920f5f4745e258830c80543e4e5a80a4fd
ShowLo <945519267@qq.com>
2020-06-10 18:32:37 +0800

    Create problem1470.py

---
+++ python/problem1470.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = [0] * (n * 2)
+        for i in range(n):
+            result[i * 2] = nums[i]
+            result[i * 2 + 1] = nums[i + n]
+        return result
